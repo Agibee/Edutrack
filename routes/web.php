277530b375e6e36785300322da19797a3af90f46
Agibee <agibpratama732@gmail.com>
2026-01-04 06:49:05 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware;
+use App\Http\Controllers\Auth\AuthController;
+
 
 Route::get('/', function () {
     return view('welcome');
